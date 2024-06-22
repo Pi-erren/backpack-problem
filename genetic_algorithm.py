bdb8752dc77f4091f6d17674e23760da25a90122
pneveu <pneveu937@gmail.com>
2024-06-22 16:48:47 +0200

    updating gen algo class

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -45,41 +45,90 @@ class GeneticAlgorithm:
             if np.random.rand() < self.mutation_rate:
                 individual[i] = 1 - individual[i]
         return individual
-
-    def repair(self, individual):
-        for i in range(len(self.budgets)):
-            while np.sum(individual * self.costs[:, i]) > self.budgets[i]:
-                included_items = individual == 1
-                utility_to_cost_ratio = self.utilities[included_items] / self.costs[included_items, i]
-                worst_item_index = np.argmin(utility_to_cost_ratio)
-                actual_worst_item_index = np.where(included_items)[0][worst_item_index]
-                individual[actual_worst_item_index] = 0
+    
+    def repair_ratio_utility_cost(self, individual):
+        total_costs = np.sum(self.costs, axis=1)  # Sum of costs across all budget categories for each item
+        while np.any(np.sum(individual[:, np.newaxis] * self.costs, axis=0) > self.budgets):
+            included_items = (individual == 1)
+            utility_to_total_cost_ratio = self.utilities[included_items] / total_costs[included_items]
+            worst_item_index = np.argmin(utility_to_total_cost_ratio)
+            actual_worst_item_index = np.where(included_items)[0][worst_item_index]
+            individual[actual_worst_item_index] = 0
         return individual
 
-    def run(self):
+    def repair_highest_utility(self, individual):
+        for i in range(len(self.budgets)):
+            while np.sum(individual * self.costs[:, i]) > self.budgets[i]:
+                worst_item_index = np.argmax(self.utilities * individual)
+                individual[worst_item_index] = 0
+        return individual
+
+    def repair_algorithm_1(self, individual):
+        # Order the objects by decreasing utility
+        ordered_indices = np.argsort(-self.utilities)
+        costs_sum = np.sum(individual[:, np.newaxis] * self.costs, axis=0)
+
+        # Remove objects if budgets are exceeded
+        for l in ordered_indices[::-1]:
+            if individual[l] == 1 and np.any(costs_sum > self.budgets):
+                individual[l] = 0
+                costs_sum -= self.costs[l]
+
+        # Try to add objects back
+        for l in ordered_indices:
+            if individual[l] == 0 and np.all(costs_sum + self.costs[l] <= self.budgets):
+                individual[l] = 1
+                costs_sum += self.costs[l]
+
+        return individual
+
+    def run(self, repair_method):
+        """
+        Run the genetic algorithm for the specified number of generations.
+
+        Parameters:
+        - repair_method: str
+            The name of the repair method to use. It should be one of the methods defined in the class.
+
+        Returns:
+        - best_solution: ndarray
+            The best solution found by the genetic algorithm.
+        """
+        # num_elites is the number of elites to carry on to the next generation
         num_elites = int(self.population_size * self.elitism_rate)
-        
-        for generation in range(self.num_generations):
+        self.best_fitness_history = []
+
+        for _ in range(self.num_generations):
             fitnesses = np.array([self.fitness(individual) for individual in self.population])
+
+            # Select the elite individuals based on their fitness
             elite_indices = np.argsort(fitnesses)[-num_elites:]
             elites = self.population[elite_indices]
+
+            # Select the rest of the population based on fitness-proportional selection. Therefore, individuals with highest fitness will have more chances to reproducing with each other
             selected_population = self.selection()
             next_population = []
-
+            
             for i in range(0, self.population_size - num_elites, 2):
+                # Reproducing
                 parent1, parent2 = selected_population[i], selected_population[i + 1]
                 offspring1, offspring2 = self.crossover(parent1, parent2), self.crossover(parent2, parent1)
+
+                # Mutation
                 offspring1, offspring2 = self.mutate(offspring1), self.mutate(offspring2)
-                offspring1, offspring2 = self.repair(offspring1), self.repair(offspring2)
+
+                # Repair
+                offspring1 = getattr(self, repair_method)(offspring1)
+                offspring2 = getattr(self, repair_method)(offspring2)
+
                 next_population.extend([offspring1, offspring2])
 
             next_population.extend(elites)
             self.population = np.array(next_population)
-            
+
             best_fitness = max(fitnesses)
             self.best_fitness_history.append(best_fitness)
+
             # print(f'Generation {generation}: Best Fitness = {best_fitness}')
 
-        best_solution = self.population[np.argmax([self.fitness(individual) for individual in self.population])]
-        
-        return best_solution
+        return self.population[np.argmax([self.fitness(individual) for individual in self.population])]
